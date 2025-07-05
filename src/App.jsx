06e34614c0f39a8fddc71d7ca5cd7c011698c30d
커밋 <>
2025-07-05 09:32:10 +0900

    0705_css수정,이미지추가,반응형추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 //import './App.css'
 import './layout.css'
 import logo from './img/logo.svg'
+import mobileLogo from './img/mobile_logo.svg'
 import profile from './img/profile.svg'
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
     // article별로 컴포넌트 분리해야 함
     <>
       <header>
-        <img className='logo' src={logo}/>
+        <picture>
+          <source media="(max-width: 768px)" srcSet={mobileLogo} />
+          <img className="logo" src={logo} alt="로고" />
+        </picture>
         <div className='login'>
           <img className='profile' src={profile}></img>
           {/* 가지고 디자인 해보세용 */}
@@ -20,22 +24,26 @@ function App() {
         <section className='mainHeader'>
           <article className='goal'>
             <div className='current'>
-              목표달성 현황
-              <div className='goalSummary'>00000원/00000원</div>
+              목표 달성 현황
+              <div className='goalSummary'>250,000원 <span>/ 500,000원</span></div>
               <div className='progressBar'></div>
             </div>
             <div className='expension'>
-              <div className='totalExpension'>이번 달 0000원</div>
-              <div className='averExpension'>지금까지 평균 0000원</div>
+              <div className='totalExpension'><span>이번 달 지출</span> 2,400,000원</div>
+              <div className='averExpension'><span>하루 평균 소비</span> 30,000원</div>
             </div>
           </article>
           <article className='expend'>
             <div className='expLayout'>{/* 레이아웃용 div 입니다 제거하셔도 됩니다 */}
               <article className='AIcomment'>
-                여기에 ai 훈수 들어갑니다
+                <h3>🍽️  이번 달 가장 많은 소비는 식비입니다</h3>
+                <p>이번 달 식비가 45%로 가장 컸어요.<br/>
+                주중 배달이 많았던 점이 눈에 띄네요!<br/>
+                다음 주엔 하루만 줄여도 2만 원을 절약할 수 있어요.</p>
+                <span>💡 1주 1회 요리 도전 → 평균 15 % 절약!</span>
               </article>
               <article className='graph'>
-                여기에 원그래프 들어갑니다
+                여기에 원그래프 들어갑니다이
               </article>
             </div>
           </article>
@@ -86,7 +94,16 @@ function App() {
         </section>
       </main>
       <footer>
-        푸터임
+        <div>
+          <p>(주)세이빗 | 대표 Thein 2Team<br/>
+          사업자등록번호 123-45-67890 | 통신판매업 신고 2025-서울강남-1234<br/>
+          서울특별시 강남구 테헤란로 123, 4층 (06123)</p>
+          <p>고객센터 1588-1234 (평일 09:00~18:00) | E-mail support@saveit.co.kr</p>
+        </div>
+        <div className='footRight'>
+          <p>이용약관 | 개인정보처리방침 | 전자금융거래약관 | 청소년보호정책</p>
+          <span>© 2025 SaveIT, Inc. All rights reserved.</span>
+        </div>
       </footer>
     </>
   )
