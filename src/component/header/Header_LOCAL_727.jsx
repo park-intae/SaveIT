4f0c2c9a1293aeab6f41e0f27f622c52309f9ce4
BOOK-GJJ3KVP6H9\pit19 <pit1995@outlook.kr>
2025-07-07 13:03:10 +0900

    error)충돌 말생해서 main으로 병합

---
+++ src/component/header/Header_LOCAL_727.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <div>
+            <img className='logo' />
+            <div className='login'>
+                {/* 가지고 디자인 해보세용 */}
+                {/* {isLogin === 'false' ? <button>구글 로그인</button>:<div><img className='profile'></img><p>{userName}</p></div>} */}
+            </div>
+        </div>
+    )
+}
