190b7f766f4f9a712ed9b0eab70d01e2e68459e9
커밋 <>
2025-07-07 17:37:57 +0900

    Refactor, Design : 목표 요약 영역에 연필 아이콘 추가,  구조 변경

--- src/component/main/mainHeader/goal/Current.jsx
+++ src/component/main/mainHeader/goal/Current.jsx
@@ -1,8 +1,13 @@
+import alter from '../../../../assets/pencil.svg'
+
 export default function Current() {
     return (
         <div className='current'>
             <p>목표달성 현황</p>
-            <div className='goalSummary'>250,000원 <span>/ 500,000원</span></div>
+            <div className='goalSummary'>
+                250,000원 <span>/ 500,000원</span>
+                <img className="alter" src={alter} alt="수정하기" />
+            </div>
             <div className='progressBar'></div>
         </div>
     )
