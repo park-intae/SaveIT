c0be7924ee4ae1f6a4b38a8991cec79c5189ad3a
커밋 <>
2025-07-08 21:44:44 +0900

    Design, Refactor: 푸터 반응형 정렬 / 스타일 구조 개선

--- src/component/footer/Right.jsx
+++ src/component/footer/Right.jsx
@@ -1,13 +1,34 @@
 import styled from "styled-components"
+import { ResponsiveContext } from '../../context/ResponsiveProvider';
+import { useContext } from 'react';
 
 const StyleRight = styled.article`
-  text-align: right;
-  font-size: 13px;
+  font-size: ${({isMobile})=>(isMobile ? "9px" : "13px")};
+  width: 100%;
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  align-items: ${({ isMobile, isTablet }) =>
+    isMobile || isTablet ? "center" : "flex-end"};
+  text-align: ${({ isMobile, isTablet }) =>
+    isMobile || isTablet ? "center" : "right"};
+
+  p{
+    display: ${({ isMobile, isTablet }) =>
+      isMobile || isTablet ? "none" : "block"};
+  }
+  span{
+    display: block;
+    text-align: center;
+    margin-top: 8px;
+  }
 `
 
 export default function Right() {
+    const {isMobile, isTablet} = useContext(ResponsiveContext);
+
     return (
-        <StyleRight className='footRight'>
+        <StyleRight isMobile={isMobile} isTablet={isTablet} className='footRight'>
             <p>이용약관 | 개인정보처리방침 | 전자금융거래약관 | 청소년보호정책</p>
             <span>© 2025 SaveIT, Inc. All rights reserved.</span>
         </StyleRight>
