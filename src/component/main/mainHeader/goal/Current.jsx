048a1a187c0374b349462de73be54a8487a32dc9
커밋 <>
2025-07-08 17:00:24 +0900

    Design: goal카드 styled-components로 구조 감싸고 폰트 스타일 적용

--- src/component/main/mainHeader/goal/Current.jsx
+++ src/component/main/mainHeader/goal/Current.jsx
@@ -1,14 +1,32 @@
 import alter from '../../../../assets/pencil.svg'
+import styled from 'styled-components';
+
+const StyleCurrent = styled.div`
+    p {
+        font-size: 18px;
+    }
+    .goalSummary {
+    font-size: 28px;
+    font-weight: bold;
+    }
+    span{
+        font-size: 15px;
+        color: #888;
+    }
+
+`
 
 export default function Current() {
     return (
-        <div className='current'>
-            <p>목표달성 현황</p>
-            <div className='goalSummary'>
-                250,000원 <span>/ 500,000원</span>
-                <img className="alter" src={alter} alt="수정하기" />
+        <StyleCurrent>
+            <div className='current'>
+                <p>목표달성 현황</p>
+                <div className='goalSummary'>
+                    250,000원 <span>/ 500,000원</span>
+                    <img className="alter" src={alter} alt="수정하기" />
+                </div>
+                <div className='progressBar'></div>
             </div>
-            <div className='progressBar'></div>
-        </div>
+        </StyleCurrent>
     )
 }
--- src/component/main/mainHeader/goal/Expension.jsx
+++ src/component/main/mainHeader/goal/Expension.jsx
@@ -3,13 +3,22 @@ import styled from "styled-components"
 const StyleExpension = styled.article`
   display: flex;
   flex-direction: column;
+
+  span{
+    font-weight: bold;
+    font-size: 15px;
+    color: #666;
+    padding-right: 30px;
+  }
 `
 
 export default function Expension() {
     return (
-        <article className='expension'>
-            <div className='totalExpension'><span>이번 달 지출</span> 2,400,000원</div>
-            <div className='averExpension'><span>하루 평균 소비</span> 30,000원</div>
-        </article>
+        <StyleExpension>
+            <article className='expension'>
+                <div className='totalExpension'><span>이번 달 지출</span> 2,400,000원</div>
+                <div className='averExpension'><span>하루 평균 소비</span> 30,000원</div>
+            </article>
+        </StyleExpension>
     )
 }
