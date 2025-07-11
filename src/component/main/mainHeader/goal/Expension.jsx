5b7a6019ea3dd389126290eb7855fbc2b47f06b7
커밋 <>
2025-07-11 09:42:31 +0900

    refactor: 불필요한 태그 구조 제거

--- src/component/main/mainHeader/goal/Expension.jsx
+++ src/component/main/mainHeader/goal/Expension.jsx
@@ -22,10 +22,8 @@ const StyleExpension = styled.article`
 export default function Expension() {
     return (
         <StyleExpension>
-            <article className='expension'>
                 <div className='totalExpension'><span>이번 달 지출</span> 2,400,000원</div>
                 <div className='averExpension'><span>하루 평균 소비</span> 30,000원</div>
-            </article>
         </StyleExpension>
     )
 }
