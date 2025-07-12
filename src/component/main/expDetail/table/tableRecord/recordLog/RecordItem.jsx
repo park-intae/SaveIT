c435ef41bd1ecb7e4e13e3cddfdb2375bae7973b
BOOK-GJJ3KVP6H9\pit19 <pit1995@outlook.kr>
2025-07-12 09:58:30 +0900

    Design: slide효과 임시
    - master pull을 위한 임시 커밋

--- src/component/main/expDetail/table/tableRecord/recordLog/RecordItem.jsx
+++ src/component/main/expDetail/table/tableRecord/recordLog/RecordItem.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 
-const StyleRecordItem = styled.div`display: flex;
+const StyleRecordItem = styled.div`
+display: flex;
 overflow: hidden;
 font-size: 13px;
 padding: 8px;
@@ -19,11 +20,19 @@ border-color: ${({ kind }) =>
   transform: translateY(-1px) translateZ(0);
 }
 
+&:hover .Ilabel span {
+  transform: translateX(-100%);
+}
+
+&:hover .Ivalue span {
+  transform: translateX(calc(50% - 100%));
+}
+
 /* .Ilabel과 .Ivalue에 공통 적용되는 스타일 */
 .Ilabel,
 .Ivalue {
   white-space: nowrap;
-  /*overflow: hidden;*/
+  overflow: hidden;
   text-overflow: ellipsis;
   position: relative;
 }
@@ -38,13 +47,6 @@ border-color: ${({ kind }) =>
   padding-left: 5px;
   flex: 0 0 auto;
   width: 40%;
-  $:hover{
-    overflow: visible;
-    text-overflow: clip;
-  }
-  &:hover span{
-    transform: translateX(calc(40% - 100%));
-  }
 }
 
 .Ivalue {
@@ -52,15 +54,70 @@ border-color: ${({ kind }) =>
   flex: 1 1 auto;
   text-align: right;
   font-size: clamp(12px, 13px, 16px);
-  $:hover{
-    overflow: visible;
-    text-overflow: clip;
-  }
-  &:hover span{
-    transform: translateX(calc(100% - 100%));
-  }
 }
 `
+// const StyleRecordItem = styled.div`
+// display: flex;
+// overflow: hidden;
+// font-size: 13px;
+// padding: 8px;
+// background-color: white;
+// border: 1px solid;
+// border-radius: 8px;
+// box-shadow: 0 2px 6px rgba(0, 0, 0, 0.05);
+// transition: box-shadow 0.3s ease, transform 0.3s ease;
+
+// /* 'kind' 값에 따라 테두리 색상 동적 변경 */
+// border-color: ${({ kind }) =>
+//         kind === "소비" ? "#e74c3c" : kind === "저축" ? "#27ae60" : "#333"};
+
+// &:hover {
+//   box-shadow: 0 4px 10px rgba(50, 50, 50, 0.1);
+//   transform: translateY(-1px) translateZ(0);
+// }
+
+// /* .Ilabel과 .Ivalue에 공통 적용되는 스타일 */
+// .Ilabel,
+// .Ivalue {
+//   white-space: nowrap;
+//   overflow: hidden;
+//   text-overflow: ellipsis;
+//   position: relative;
+// }
+
+// .Ilabel span,
+// .Ivalue span{
+//     display: inline-block;
+//     transition: transform 1s ease-out;
+//     }
+
+// .Ilabel {
+//   padding-left: 5px;
+//   flex: 0 0 auto;
+//   width: 40%;
+//   $:hover{
+//     overflow: visible;
+//     text-overflow: clip;
+//   }
+//   &:hover span{
+//     transform: translateX(-100%);
+//   }
+// }
+
+// .Ivalue {
+//   padding-right: 5px;
+//   flex: 1 1 auto;
+//   text-align: right;
+//   font-size: clamp(12px, 13px, 16px);
+//   $:hover{
+//     overflow: visible;
+//     text-overflow: clip;
+//   }
+//   &:hover span{
+//     transform: translateX(calc(50% - 100%));
+//   }
+// }
+// `
 
 export default function RecordItem({ kind, category, amount }) {
     return (
