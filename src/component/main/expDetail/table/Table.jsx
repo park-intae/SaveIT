09497e814aab38396c0fe2d1468dd01974eae548
pooperilla <pooperilla@gmail.com>
2025-07-24 10:34:36 +0900

    Refactor: 캘린더에서 expense 가져오기

--- src/component/main/expDetail/table/Table.jsx
+++ src/component/main/expDetail/table/Table.jsx
@@ -15,14 +15,14 @@ const TableCard = styled(StyleCard)`
     }
 `
 
-console.log("weeklyRecords", weeklyRecords);
 
 export default function Table() {
-    const {weeklyRecords, fetchWeeklyRecords, isLoading, error} = useWeeklyStore();
+    const {fetchExpense, expenseData, isLoading, error} = useWeeklyStore();
 
     useEffect( () => {
-        fetchWeeklyRecords();
+        fetchExpense();
     },[]);
+    console.log("expenseData", expenseData);
 
     if(isLoading) return '데이터를 불러오는 중입니다';
     if(error) return <p>에러발생: {error}</p>
@@ -30,8 +30,8 @@ export default function Table() {
     return (
         <>
             <TableCard className="table">
-                {weeklyRecords.map((record, index) => ( //weeklyRecords는 목업 데이터임 나중에 서버에서 데이터 받아와야됨
-                    <TableRecord key={index} {...record}/>
+                {expenseData.map((expense, index) => ( //weeklyRecords는 목업 데이터임 나중에 서버에서 데이터 받아와야됨
+                    <TableRecord key={index} {...expense}/>
                 ))}
             </TableCard>
         </>
